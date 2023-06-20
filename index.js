18e2866fb1a0c7c013bb4b5c6378134fc751c218
RahulPoddar1602 <rahul1999p@gmail.com>
2023-06-20 22:45:51 +0530

    bugs fixed 1.0

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express=require("express")
 const app=express()
+// const cin=require("circular-json")
 
 const path=require("path")
 const http=require("http")
 const {Server}=require("socket.io")
-
+const port = process.env.PORT || 3000;
 
 const server=http.createServer(app)
 
@@ -13,18 +14,45 @@ app.use(express.static(path.resolve("")))
 
 let arr=[]
 let playingArray=[]
-
-io.on("connection",(socket)=>{
+let roomId="";
+// let val=0;
+io.on("connection",async(socket)=>{
     console.log("rahul");
     socket.on("he",(e)=>{
         console.log("rahulpoddar")
         console.log(e.name)
     })
-    socket.on("left",(e)=>{
-        io.emit("left",e)
+    // await socket.on("joinRoom",(e)=>{
+    //     if(e.roomId!=null)
+    //     {
+    //         socket.join(e.roomId);
+    //         roomId=e.roomId
+    //     }
+    // })
+    // val++;
+    // if(val>=2)
+    // {
+    //     val=0;
+    //     roomId++;    
+    // }
+    // socket.join(roomId)
+    // io.emit("roomno",{no:roomId})
+    socket.on("moving",(e)=>{
+        // io.emit("left",e)
+        // console.log(e)
+        io.emit("moving",e)
+    })
+    socket.on("moving1",(e)=>{
+        // io.emit("left",e)
+        // console.log(e)
+        io.emit("moving1",e)
     })
     socket.on("right",(e)=>{
         io.emit("right",e)
+    })
+    socket.on("left",(e)=>{
+        io.emit("left",e)
+        // console.log(e)
     })
     socket.on("up",(e)=>{
         io.emit("up",e)
@@ -56,6 +84,22 @@ io.on("connection",(socket)=>{
     socket.on("/",(e)=>{
         io.emit("/",e)
     })
+    socket.on("bulletbreak",(e)=>{
+        io.emit("bulletbreak",e)
+    })
+    socket.on("bullet1break",(e)=>{
+        io.emit("bulletbreak",e)
+    })
+    socket.on("tankIron",(e)=>{
+        io.emit("tankIron",e)
+    })
+    socket.on("tank2death",(e)=>{
+        io.emit("tank2death",e)
+    })
+    socket.on("tank1death",(e)=>{
+        io.emit("tank1death",e)
+    })
+
 })
 
 
@@ -64,6 +108,6 @@ app.get("/",(req,res)=>{
     return res.sendFile("index.html")
 })
 
-server.listen(3000,()=>{
+server.listen(port,()=>{
     console.log("port connected to 3000")
 })
--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const socket = io();
 socket.emit("he",{name:"poddar"})
-// socket.on("she",(d)=>{
-//     document.write(d.name)
-// })
 kaboom({
     width: 1000,
     height: 500,
@@ -13,18 +10,6 @@ kaboom({
 })
 
 const canvas= document.querySelector("canvas")
-// canvas.style.display ="none"
-
-// setTimeout(()=>{
-    //     canvas.style.display ="block"
-    // loadData().then(()=>{
-    //     start()
-    // })
-    
-// },10000)
-
-
-// ()=> ({ 
     const loadData = async ()=>{
         await loadSprite("tank1up", "assets/player1_tank_up.png")
         await loadSprite("tank1down", "assets/player1_tank_down.png")
@@ -73,17 +58,20 @@ const canvas= document.querySelector("canvas")
 })
 //game scene
 scene("game", () => {
-    add([
-        text("Instructions\n\nMoves\n\nPlayer1\nmovement : up, down, left, right\nshooting: space bar\nreload: /\n\nPlayer2\nmovement : w, s, a, d\nshooting: k\nreload: r \n\nIt will take 3 seconds to reload.", { size: 20  }),
-        pos(width() / 2, height() / 2),
-        anchor("center"),
-        z(100),
-        lifespan(10),
-        fixed(),
-    ])
-    add([
-        rect(width()/2,height()),
-        pos(width() / 2, height() / 2),
+    // console.log(get("game"))
+    // const objects = getObjects();
+// console.log(objects);
+add([
+    text("Instructions\n\nMoves\n\nPlayer1\nmovement : up, down, left, right\nshooting: space bar\nreload: /\n\nPlayer2\nmovement : w, s, a, d\nshooting: k\nreload: r \n\nIt will take 3 seconds to reload.", { size: 20  }),
+    pos(width() / 2, height() / 2),
+    anchor("center"),
+    z(100),
+    lifespan(10),
+    fixed(),
+])
+add([
+    rect(width()/2,height()),
+    pos(width() / 2, height() / 2),
         anchor("center"),
         z(99),
         color(9,23,45),
@@ -92,7 +80,7 @@ scene("game", () => {
     ])
     let t1=0;
     let t2=1;
-    const SPEED = 100
+    const SPEED = 50
     const BULLET_SPEED = 500
     const breakHealth = 3
     const tank1Health = 20
@@ -109,7 +97,7 @@ scene("game", () => {
         "=@@@@@@@ #@ @      =",
         "=   =  @ @@@@@@=   =",
         "=   =  @  @+=  =   =",
-            "=====================",
+        "=====================",
         ]
         const level = addLevel(LEVELS, {
             tileWidth: 50,
@@ -146,32 +134,75 @@ scene("game", () => {
                 
             },
         })
+        // console.log(obj)
         let isSpacePressed = false;
         const player1 = add([
             sprite("tank1down"),
             pos(100, 80),
             rotate(0),
             area(),
-            body(),
+            body({mass:100}),
             health(tank1Health),
             anchor("center"),
-            "tank1"
+            "tank1",
+            "tank"
         ])
         const player2 = add([
             sprite("tank2up"),   
             pos(width()-120,height()-80),     
-            body(),
+            body({mass:100}),
             area(),
             rotate(0),        
             anchor("center"), 
             health(tank2Health),
-            "tank2"
+            "tank2",
+            "tank"
         ])
+        socket.on("moving",(e)=>{
+            player1.pos.x=e.axis.x
+            player1.pos.y=e.axis.y
+        })
+        socket.on("moving1",(e)=>{
+            player2.pos.x=e.axis.x
+            player2.pos.y=e.axis.y
+        })
         // player 1 movement
         onKeyDown("left", () => {
             socket.emit("left",{tank:'tank1left',x:SPEED,y:0,t1:3})
             // player1.use(sprite('tank1left'))
             // t1=3
+        })
+        onKeyRelease("left",()=>{
+            let e= get("tank1")[0]
+            socket.emit("moving",{axis:e.pos})
+        })
+        onKeyRelease("right",()=>{
+            let e= get("tank1")[0]
+            socket.emit("moving",{axis:e.pos})
+        })
+        onKeyRelease("up",()=>{
+            let e= get("tank1")[0]
+            socket.emit("moving",{axis:e.pos})
+        })
+        onKeyRelease("down",()=>{
+            let e= get("tank1")[0]
+            socket.emit("moving",{axis:e.pos})
+        })
+        onKeyRelease("w",()=>{
+            let e= get("tank2")[0]
+            socket.emit("moving1",{axis:e.pos})
+        })
+        onKeyRelease("a",()=>{
+            let e= get("tank2")[0]
+            socket.emit("moving1",{axis:e.pos})
+        })
+        onKeyRelease("s",()=>{
+            let e= get("tank2")[0]
+            socket.emit("moving1",{axis:e.pos})
+        })
+        onKeyRelease("d",()=>{
+            let e= get("tank2")[0]
+            socket.emit("moving1",{axis:e.pos})
         })
         socket.on("left",(e)=>{
             player1.use(sprite(e.tank))
@@ -180,9 +211,6 @@ scene("game", () => {
         })
         onKeyDown("right", () => {
             socket.emit("right",{tank:'tank1right',x:SPEED,y:0,t1:2})
-            // player1.use(sprite('tank1right'))
-            // player1.move(SPEED, 0)
-            // t1=2
         })
         socket.on("right",(e)=>{
             player1.use(sprite(e.tank))
@@ -191,9 +219,6 @@ scene("game", () => {
         })
         onKeyDown("up", () => {
             socket.emit("up",{tank:'tank1up',y:SPEED,x:0,t1:1})
-            // player1.use(sprite('tank1up'))
-            // player1.move(0, -SPEED)
-            // t1=1
         })      
         socket.on("up",(e)=>{
             player1.use(sprite(e.tank))
@@ -202,9 +227,6 @@ scene("game", () => {
         })
         onKeyDown("down", () => {
             socket.emit("down",{tank:'tank1down',y:SPEED,x:0,t1:0})
-            // player1.use(sprite('tank1down'))
-            // player1.move(0, SPEED)
-            // t1=0
         })
         socket.on("down",(e)=>{
             player1.use(sprite(e.tank))
@@ -214,9 +236,6 @@ scene("game", () => {
         // player 2 movement
         onKeyDown("a", () => {
             socket.emit("a",{tank:'tank2left',x:SPEED,y:0,t2:3})
-            // player2.use(sprite('tank2left'))
-            // player2.move(-SPEED, 0)
-            // t2=3
         })
         socket.on("a",(e)=>{
             player2.use(sprite(e.tank))
@@ -225,9 +244,6 @@ scene("game", () => {
         })
         onKeyDown("d", () => {
             socket.emit("d",{tank:'tank2right',x:SPEED,y:0,t2:2})
-            // player2.use(sprite('tank2right'))
-            // player2.move(SPEED, 0)
-            // t2=2        
         })
         socket.on("d",(e)=>{
             player2.use(sprite(e.tank))
@@ -236,9 +252,6 @@ scene("game", () => {
         })
         onKeyDown("w", () => {
             socket.emit("w",{tank:'tank2up',y:SPEED,x:0,t2:1})
-            // player2.use(sprite('tank2up'))
-            // player2.move(0, -SPEED)
-            // t2=1
         })
         socket.on("w",(e)=>{
             player2.use(sprite(e.tank))
@@ -247,9 +260,6 @@ scene("game", () => {
         })
         onKeyDown("s", () => {
             socket.emit("s",{tank:'tank2down',y:SPEED,x:0,t2:0})
-            // player2.use(sprite('tank2down'))
-            // player2.move(0, SPEED)
-            // t2=0
         })
         socket.on("s",(e)=>{
             player2.use(sprite(e.tank))
@@ -261,68 +271,43 @@ scene("game", () => {
             if(q===0)
             add([
                 sprite("Bullet"),
-                // rect(6, 12),
                 area(),
                 pos(p),
                 anchor("center"),
-                // color(127, 127, 255),
-                // outline(4),
                 move(DOWN, BULLET_SPEED),
                 offscreen({ destroy: true }),
                 "bullet",
-                {
-                    dir:1,
-                }
-            
+                
             ])
             else if(q===1)
             add([
                 sprite("Bullet"),
-                // rect(6, 12),
                 area(),
                 pos(p),
                 anchor("center"),
-                // color(127, 127, 255),
-                // outline(4),
                 move(UP, BULLET_SPEED),
                 offscreen({ destroy: true }),
                 "bullet",
-                {
-                    dir:1,
-                }
             ])
             else if(q===2)
             add([
                 sprite("Bullet"),
-                // rect(12, 6),
                 area(),
                 pos(p),
                 anchor("center"),
-                // color(127, 127, 255),
-                // outline(4),
                 move(RIGHT, BULLET_SPEED),
                 offscreen({ destroy: true }),
                 "bullet",
-                {
-                    dir:1,
-                }
             ])
             else
             add([
                 sprite("Bullet"),
-                // rect(12, 6),
                 area(),
-                body(),
                 pos(p),
                 anchor("center"),
-                // color(127, 127, 255),
-                // outline(4),
                 move(LEFT, BULLET_SPEED),
                 offscreen({ destroy: true }),
                 "bullet",
-                {
-                    dir:1,
-                }
             ])
         }
         //player 2 bullet
@@ -330,12 +315,9 @@ scene("game", () => {
             if(q===0)
             add([
                 sprite("Bullet"),
-                // rect(6, 12),
                 area(),
                 pos(p),
                 anchor("center"),
-                // color(127, 127, 255),
-                // outline(4),
                 move(DOWN, BULLET_SPEED),
                 offscreen({ destroy: true }),
                 "bullet1",
@@ -343,55 +325,69 @@ scene("game", () => {
             else if(q===1)
             add([
                 sprite("Bullet"),
-                // rect(6, 12),
                 area(),
                 pos(p),
                 anchor("center"),
-                // color(127, 127, 255),
-                // outline(4),
                 move(UP, BULLET_SPEED),
                 offscreen({ destroy: true }),
                 "bullet1",
             ])
             else if(q===2)
             add([
-                // rect(12, 6),
                 sprite("Bullet"),
                 area(),
                 pos(p),
                 anchor("center"),
-                // color(127, 127, 255),
-                // outline(4),
                 move(RIGHT, BULLET_SPEED),
                 offscreen({ destroy: true }),
                 "bullet1",
             ])
             else
             add([
-                // rect(12, 6),
                 sprite("Bullet"),
                 area(),
-                body(),
                 pos(p),
                 anchor("center"),
-                // color(127, 127, 255),
-                // outline(4),
                 move(LEFT, BULLET_SPEED),
                 offscreen({ destroy: true }),
                 "bullet1",
             ])
         }
+        // const breakElements = get("break");
         //bullet collision with bricks
-        onCollide("bullet", "break", (b, e) => {
-            e.hurt(1)
-            if(e.hp()==0)
+        onCollide("bullet", "break", (b, d) => {
+            socket.emit("bulletbreak",{bulletId:b.id,brickId:d.id})
+        })
+        socket.on("bulletbreak",(e)=>{
+            const brick=get("*", { recursive: true }).find(o => o.id === e.brickId)
+            const bullet=get("*", { recursive: true }).find(o => o.id === e.bulletId)
+            console.log(brick)
+            console.log(bullet)
+            brick && brick.hurt(1);
+            if(brick && brick.hp()==0)
             {
-                destroy(e);
+                brick && destroy(brick);
                 play("bricks")
             }
-            console.log(e.hp())
-            destroy(b)
+            bullet && destroy(bullet);
         })
+        onCollide("bullet1", "break", (b, d) => {
+            socket.emit("bullet1break",{bulletId:b.id,brickId:d.id})
+        })
+        socket.on("bullet1break",(e)=>{
+            const brick=get("*", { recursive: true }).find(o => o.id === e.brickId)
+            const bullet=get("*", { recursive: true }).find(o => o.id === e.bulletId)
+            console.log(brick)
+            console.log(bullet)
+            brick && brick.hurt(1);
+            if(brick && brick.hp()==0)
+            {
+                brick && destroy(brick);
+                play("bricks")
+            }
+            bullet && destroy(bullet);
+        })
+        
         onCollide("bullet1", "break", (b, e) => {
             e.hurt(1)
             if(e.hp()==0){
@@ -405,38 +401,33 @@ scene("game", () => {
         onCollide("bullet", "tank2", (b, e) => {
             e.hurt(1)
             if(e.hp()==0)
-            destroy(e);
-            // console.log(e.hp())
+            socket.emit("tank2death",{})
+            // destroy(e);
             destroy(b)
+        })
+        socket.on("tank2death",()=>{
+            destroy(get("tank2"));
         })
         onCollide("bullet1", "tank1", (b, e) => {
             e.hurt(1)
             if(e.hp()==0)
-            destroy(e);
-            // console.log(e.hp())
+            socket.emit("tank1death",{})
+            // destroy(e);
             destroy(b)
         })
-        //tanks when hurt
-        on("hurt", "tank2", (e) => {
-            shake(1)
-            play("thud")
+        socket.on("tank1death",()=>{
+            destroy(get("tank1"));
         })
-        on("hurt", "tank1", (e) => {
+        //tanks when hurt
+        on("hurt", "tank", (e) => {
             shake(1)
             play("thud")
         })
         //tanks on death
-        on("death", "tank1", (e) => {
+        on("death", "tank", (e) => {
             shake(3)
             play("explosion")
             addKaboom(e.pos)
-            // alert("game over! Player 2 wins")
-        })
-        on("death", "tank2",(e) => {
-            shake(3)
-            play("explosion")
-            addKaboom(e.pos)
-            // alert("game over! Player 1 wins")
         })
         //tanks collision
         onCollide("tank1","tank2",()=>{
@@ -444,19 +435,12 @@ scene("game", () => {
             shake(2)
         })
         //tanks powerup
-        onCollide("helth","tank1",(h,e)=>{
+        onCollide("helth","tank",(h,e)=>{
             e.heal(20-e.hp())
             play("helth")
             h.destroy()
         })
-        onCollide("helth","tank2",(h,e)=>{
-            e.heal(20-e.hp())
-            play("helth")
-            h.destroy()
-        })
-        //bullet on white bricks
         onCollide("bullet", "steel", (b, e) => {
-            console.log(b)
             destroy(b)
         })
         onCollide("bullet1", "steel", (b, e) => {
@@ -464,14 +448,23 @@ scene("game", () => {
         })
         //bullet on steel
         onCollide("bullet", "Iron", (b, e) => {
-            // b.dir=-1;
-            // b.move(BULLET_SPEED*b.dir,0)
             play("pang")
             destroy(b)
         })
+        
         onCollide("bullet1", "Iron", (b, e) => {
             play("pang")
             destroy(b)
+        })
+        onCollideEnd("tank","Iron", (b,e)=>{
+            console.log(b)
+            console.log(e)
+            socket.emit("tankIron",{id:e.id})
+        })
+        socket.on("tankIron",(e)=>{
+            const tank=get("*", { recursive: true }).find(o => o.id === e.id)
+            // console.log(tank)
+            tank && tank.move(tank.dir)
         })
         //player1 shooting and reloading
         onKeyPress("space", () => {
@@ -618,9 +611,6 @@ scene("game", () => {
                 fixed(),
             ])
             
-            // player.onCollide("coin", () => {
-                //     score.value += 1
-                // })
 })
 //start function
 function start() {
